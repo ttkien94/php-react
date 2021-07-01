1214d7dcf5c1d4cf011b58240cc6a394cb25717c
ttkien94 <82698676+ttkien94@users.noreply.github.com>
2021-07-01 08:49:25 +0700

    php-react

---
+++ php-react/db_connection.php
@@ -0,0 +1,4 @@
+<?php
+$db_conn = mysqli_connect("localhost","root","","react_php_crud");
+mysqli_set_charset($db_conn,'utf8');
+?>
---
+++ php-react/menu.php
@@ -0,0 +1,18 @@
+<?
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Content-Type: application/json; charset=UTF-8");
+
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers,Authorization, X-Requested-With");
+
+require 'db_connection.php';
+
+$sql = mysqli_query($db_conn,"select * from menu");
+if(mysqli_num_rows($sql)>0){
+    $sql = mysqli_fetch_all($sql,MYSQLI_ASSOC);
+    echo json_encode(['success'=>1,"menu"=>$sql]);
+}else{
+    echo json_encode(["success"=>0]);
+}
+?>
